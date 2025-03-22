61297a4bb3b5665c3783a2df7da78f7100f7ab8f
Walter Dudnic <walttinhu@hotmail.com>
2025-03-22 17:46:19 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,20 @@
+function adicionarAnimacao() {
+  let img = document.getElementById("imagem");
+  
+  // Adiciona a classe para ativar a animação
+  img.classList.add("flutuando");
+}
+
+// function removeAnimacao() {
+//   let img = document.getElementById("imagem");
+
+//   // Remove a classe para parar a animação ao tirar o mouse
+//   img.classList.remove("flutuando");
+// }
+
+
+
+
+
+
+
